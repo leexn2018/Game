7151effe49d5f15ba6a5d8ddc39e255def8a5c36
leexn2018 <Leexn2018@qq.com>
2022-09-14 14:12:54 +0800

    11111

---
+++ src/App.vue
@@ -0,0 +1,51 @@
+<template>
+  <div id="loading">
+    <h1> loading </h1>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { onMounted } from 'vue'
+import { Game } from "./assets/js/class.js"
+
+onMounted(() => {
+  //delete loading view
+  console.log("loaded");
+  let loading = document.querySelector("#loading")
+  loading?.parentNode?.removeChild(loading)
+
+  //set view
+
+  //ability define
+  const lifeImprove: ability = {
+    name: "lifeImprove", description: "will have a age increase", probability: 0.00001, method: (babe: any) => { babe.max_age += 1 }
+  }
+  const someab: ability = {
+    name: "tmp", description: "tmp", probability: 0.01
+  }
+
+  const config = { birthRate: 0.5, maxMember: 23 }
+
+  window.game = new Game()
+  game.regAbility(lifeImprove)
+  game.regAbility(someab)
+
+  game.start(config)
+})
+
+
+//test code
+
+</script>
+<style scoped>
+#loading {
+  margin: 40vh 33vw;
+}
+
+#loading>div {
+  margin: 0;
+}
+body {
+  margin: 0
+}
+</style>
---
+++ todo.md
@@ -0,0 +1,42 @@
+babe will die if has no bro nearby for 3 rounds
+
+
+
+game >  findFamilyByName
+        findAbilityByName
+
+
+
+colot > babe done
+
+坐标系 偏移 概率生成
+        x -0.5
+        y +0.5
+blockIndex
+0 1 2 3 4 5 ... 15
+...
+
+block
+0 1 2 3 4 5 .... 15
+...
+
+
+todo:   
+        1. ability
+        2. mutation
+        3. IF SOME ability over 70%  add to familyAbility
+        4. swim to target nearby   r = 6
+        5. 集群 ability
+bug: 
+        1.执行一定次数nextRound后 
+        Uncaught TypeError: Cannot read properties of undefined (reading 'age')   class.js:84 
+         at Game.nextRound (class.js:84:25)
+         at HTMLDocument.<anonymous> (class.js:75:22)
+
+         fixed maybe
+
+        2. 后期 绿色颜色 不显示
+
+         莫名其妙地消失了 ！
+
+        3. maxMember >= 133 时 后续消失
